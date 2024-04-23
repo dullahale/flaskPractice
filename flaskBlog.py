b7ff1c404a6bfa6595e83f0264658626d240effb
dullahale <83802559+dullahale@users.noreply.github.com>
2024-04-23 11:45:52 -0600

    Created dummy data for the blog

--- flaskBlog.py
+++ flaskBlog.py
@@ -2,6 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Dullah Ale',
+        'title': 'First Blog Post',
+        'content': 'First post content',
+        'date_posted': 'April 22, 2024',
+    },
+
+     {
+        'author': 'Chase Corey',
+        'title': 'Second Blog Post',
+        'content': 'Second post content',
+        'date_posted': 'April 23, 2024',
+    },
+
+    {
+        'author': 'Mike Blake',
+        'title': 'Third Blog Post',
+        'content': 'Third post content',
+        'date_posted': 'April 20, 2024',
+    },
+]
+
+
 @app.route("/")
 @app.route("/home")
 def home():
