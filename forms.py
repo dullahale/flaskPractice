b4ad42c9234ffacc89395370e3fb4cff7170ffd1
dullahale <83802559+dullahale@users.noreply.github.com>
2024-04-30 11:50:36 -0600

    added a registration form for new users to register and create new accounts

--- forms.py
+++ forms.py
@@ -4,4 +4,23 @@ from flask_wtf import Flaskform
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+class RegistrationForm(Flaskform):
+
+    first_name = StringField("First Name", 
+                             validators=[DataRequired(), Length(min=2, max=20)])
+    
+    last_name = StringField("Last Name", 
+                             validators=[DataRequired(), Length(min=2, max=20)])
+    
+    username = StringField("Username", 
+                             validators=[DataRequired(), Length(min=2, max=20)])
+    
+    email = StringField("Email", validators=[DataRequired(), Email()])
+
+    password = PasswordField("Password", validators=[DataRequired()])
+
+    confirm_password = PasswordField("Confirm password", validators=[DataRequired(), EqualTo("passsword")])
+
+    submit = SubmitField("Register")
+
 
