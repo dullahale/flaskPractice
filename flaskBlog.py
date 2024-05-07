f572a37e2acf84ce2b5098ffa2549353cfc10a6e
dullahale <83802559+dullahale@users.noreply.github.com>
2024-05-07 13:16:59 -0600

    added users and posts in the database in the instance folder in the site file

--- flaskBlog.py
+++ flaskBlog.py
@@ -6,6 +6,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "56de93dfdddc2a8d8d4d9e41e907c6d8"
+# SQLAlchemy a flask extension that simplifies database integration 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 
